a1710785c3e9576bafdf5f1581bc8c728ec0b3d3
nalyd17 <dylan.p.brown@gmail.com>
2025-01-29 12:36:49 -0600

    Downgrade to React@18 (19 is still in beta)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 function App() {
   return (
-    <div className='min-h-screen bg-gray-50'>
-      <main className='container mx-auto px-4 sm:px-6 lg:px-8 py-12'>
-        <div className='max-w-7xl mx-auto'>
-          <h1 className='text-4xl font-bold text-gray-900 mb-8'>
-            Welcome to Your App
-          </h1>
-          <p className='text-lg text-gray-600'>
-            Start building something amazing!
-          </p>
-        </div>
-      </main>
+    <div className='h-screen flex flex-col bg-gray-50 items-center justify-center'>
+      <span className='text-2xl font-bold'>Welcome to Your App</span>
+      <span className='text-lg font-semibold'>
+        Start building something amazing!
+      </span>
     </div>
   );
 }
